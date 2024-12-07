a6b77a8c7b0afd11060be85dbaf43a88e06fa09e
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-07 09:16:49 +0545

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        int froCommit;
+        int froCommit = 66;
+        System.out.println(froCommit);
     }
 }
